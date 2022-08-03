99d600e632d5b730e7f91d7413246a5ed454e3e1
Jhih Yang <jerry0610804.eed06@nctu.edu.tw>
2022-08-03 22:13:18 +0800

    Update ui_control.py

--- scripts/user_interface/ui_control.py
+++ scripts/user_interface/ui_control.py
@@ -304,7 +304,7 @@ class MyWidget(QWidget):
         
     # (圖片) 電池
     def showImage_battery(self):
-        pixmap = QPixmap('/airpocart_ws/scripts/user_interface/Image/battery_charge.png')
+        pixmap = QPixmap('~/airpocart_ws/scripts/user_interface/Image/battery_charge.png')
         self.battery.setPixmap(pixmap)
         self.battery.move(1800,5)
         self.battery.resize(60,30)
