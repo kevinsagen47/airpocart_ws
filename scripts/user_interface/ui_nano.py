e7b29b8673c8824deb76841b676dc6c5a0333a0f
Jhih Yang <jerry0610804.eed06@nctu.edu.tw>
2022-08-09 22:23:07 +0800

    Update ui_nano.py

--- scripts/user_interface/ui_nano.py
+++ scripts/user_interface/ui_nano.py
@@ -924,8 +924,8 @@ class MyWidget(QWidget):
             self.follow_on_btn.setEnabled(True)
         if self.follow_flag == False:
             #self.folw_th = threading.start_new_thread(follower,(self.pub,self.onn[0]))
-            threading.Thread(target=follower,args=(self.pub,self.onn[0],))
-            
+            self.folw_th = threading.Thread(target=follower,args=(self.pub,self.onn[0],))
+            self.folw_th.start()
             self.follow_flag = True
             print("follow ONNNNNNNNNNNNNNNNNNNNNN")
         else:
